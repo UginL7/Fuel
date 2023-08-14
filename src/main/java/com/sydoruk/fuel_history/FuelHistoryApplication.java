ace3f0bb69ca091448126494d1532ffde27e2751
UginL7 <outssider@gmail.com>
2023-08-14 13:06:37 +0300

    just created

---
+++ src/main/java/com/sydoruk/fuel_history/FuelHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.sydoruk.fuel_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sydoruk/fuel_history/FuelHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydoruk.fuel_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
